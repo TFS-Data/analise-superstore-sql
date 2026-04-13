9966d9b5bfa82eac39b78ce5902fa2cd294cf8c4
Thiago França <tfs.thiago@icloud.com>
2026-04-12 21:09:11 -0300

    Add superstore table
    Table created with sales, profit, discount and region columns

---
+++ sql/create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE superstore (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ship_mode VARCHAR(50),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    postal_code INT,
+    region VARCHAR(50),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(4,2),
+    profit DECIMAL(10,2)
+);
